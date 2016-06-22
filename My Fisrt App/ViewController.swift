5d4f1c8cfc58a08019fc601ea2e77212916c0650
masterUNG <nattapon_pook@hotmail.com>
2016-06-22 11:58:23 +0700

    print number

--- My Fisrt App/ViewController.swift
+++ My Fisrt App/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var numberLable: UILabel!  ///ประกาศตัวแปร
+    
+    ///Exelict ชื่อตัวแปร
+    
+    var intNuber:Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    } /// Main Function
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }  // Second Finction
+
+    
+    
+    @IBAction func IncreaseButton(sender: AnyObject) {
+        intNuber += 1
+        showNumber(intNuber)
+        
+        } /// Increase Function ปุ่มเพิ่ม
+    
+    @IBAction func DecreaseButton(sender: AnyObject) {
+        intNuber -= 1
+        showNumber(intNuber)
+        } /// Decrease Function ปุ่มลด
+    
+    func showNumber(myNumber:Int) -> Void {
+        print("myNumber = \(myNumber)")
     }
-
-
-}
+    
+    
+    
+} /// Mian Class
 
